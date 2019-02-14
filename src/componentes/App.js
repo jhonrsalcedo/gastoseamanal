fd80328d8445ec79b9e9b29b47464b72c04ba05e
jhonrsalcedo <jhonfor@gmail.com>
2019-02-14 14:48:02 -0500

    cambio componente

--- src/componentes/App.js
+++ src/componentes/App.js
@@ -1,46 +1,9 @@
 import React, { Component } from 'react';
 import Header from './Header';
 import Formulario from './Formulario';
-import Listado from './Listado';
+// import Listado from './Listado';
 
 class App extends Component {
-  //crear el state se puede de dos formas 
-
-  // constructor(prosp){
-  //   super(prosp);
-  //   // donde state siempre es el objeto
-  //   this.state = {
-  //     presupueto: '',
-  //     restante: '',
-  //     gastos: {}
-  //   }
-  // }
-  state ={
-        presupuesto: '',
-        restante: '',
-        gastos: {}
-      }
-
-
-
-// agregar un nuevo gasto al state
-  agregarGasto = gasto =>{
-    // tomar una copia del state actual
-    const gastos = {...this.props.gastos};
-
-
-    // console.log('Se agrego el gasto'+ gasto)
-    // console.log(gastos);
-  // agregar al gasto al objeto del state
-    gastos[`gasto${Date.now()}`] = gasto;
-
-    // console.log(gastos);
-  //ponerlo en state
-    this.setState({
-      gastos
-    })
-
-  }
 
 
   render() {
@@ -55,17 +18,14 @@ class App extends Component {
        <div className="row jumbotron">
         <div className="col-md-6">
         <Formulario
-        // en los formularios podemos pasar funciones, state
-        //podemos nombrar el props con el mismo nombre del metodo para no confundirse
-        agregarGasto = {this.agregarGasto}
+  
 
         />
         
         </div>
         <div className="col-md-6">
-        <Listado
-          gastos={this.state.gastos}
-        />
+     
+        <div/>
         
         </div>
        
